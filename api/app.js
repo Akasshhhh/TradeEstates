39c7945fbe7e009b0b0a4373dd0e0cec2f603f36
Akasshhhh <akashisntreal@gmail.com>
2024-11-14 00:00:25 +0530

    added ui

--- api/app.js
+++ api/app.js
@@ -1,15 +1,24 @@
 import express from "express";
 import postRoutes from "./routes/post.route.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import testRoutes from "./routes/test.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express()
 
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true})) //We need credentials as true if we are going to use cookies
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/posts', postRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/test', testRoutes)
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
+
 })
 
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js"
 
-export const register = async (req,res) =>{
-    const {username, email, password} = req.body
-    try{
+export const register = async (req, res) => {
+    const { username, email, password } = req.body
+    try {
 
         //Hash the password
         const hashedPass = await bcrypt.hash(password, 10)
@@ -15,37 +16,48 @@ export const register = async (req,res) =>{
                 password: hashedPass
             }
         })
-        
-        res.status(201).json({message: "User created"})
+
+        res.status(201).json({ message: "User created" })
         console.log(newUser)
-    } catch(error){
+    } catch (error) {
         console.log(error)
-        res.status(500).json({message: "Failed to create user"})
+        res.status(500).json({ message: "Failed to create user" })
     }
 }
-export const login = async (req,res) =>{
-    const {username, password} = req.body
+export const login = async (req, res) => {
+    const { username, password } = req.body
 
 
     try {
         //Check if the user exists
         const user = await prisma.user.findUnique({
-            where: {username}
+            where: { username }
         })
 
-        if(!user) return res.status(401).json({"message": "Invalid credentials!"})
+        if (!user) return res.status(401).json({ "message": "Invalid credentials!" })
         //Check the user password if exists
-        
+
         const isPassValid = await bcrypt.compare(password, user.password)
-        if(!isPassValid) return res.status(401).json({"message": "Invalid credentials!"})
-    
+        if (!isPassValid) return res.status(401).json({ "message": "Invalid credentials!" })
+
         //If the password is correct, generate a cookie token and send it to the user
+
+        // res.setHeader("Set-Cookie","test=" + "myValue").json("success")
+        const AGE = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign({
+            id:user.id
+        },process.env.JWT_SECRET_KEY,{expiresIn: AGE})
+
+        const {password:userPasword, ... userInfo} = user
         
+        res.cookie("token", token, { httpOnly: true, maxAge: AGE }).status(200).json(userInfo)
+
     } catch (error) {
         console.log(error)
-        res.status(500).json({"message" : "Failed to login!"})
+        res.status(500).json({ "message": "Failed to login!" })
     }
 }
-export const logout = (req,res) =>{
-
+export const logout = (req, res) => {
+    res.clearCookie("token").status(200).json({"message" : "Logout successful"})
 }
