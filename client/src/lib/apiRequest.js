4c5507e4b81f2cdd1b968d9325d1c06183c8f67b
Akasshhhh <akashisntreal@gmail.com>
2025-04-08 23:09:05 +0530

    url changes

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL
+const baseURL = import.meta.env.VITE_API_URL + "/api"
 
 const apiRequest = axios.create({
     baseURL: baseURL,
